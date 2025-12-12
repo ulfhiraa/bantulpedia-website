6b22d2a3e066c05d9fd8d189a0cae802db080799
ulfhiraa <studyiraa@gmail.com>
2025-12-12 10:25:28 +0700

    UPDATE - Tampilan home

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function SearchBar({ onSearch }) {
   return (
     <div className="flex items-center gap-3 w-full">
-      <input
+      {/* <input
         type="text"
         placeholder="Cari Layanan..."
         className="flex-1 py-3 px-4 rounded-full outline-none border border-white/20 bg-white/95 placeholder:text-slate-500 shadow-sm"
@@ -14,7 +14,7 @@ export default function SearchBar({ onSearch }) {
         className="py-2 px-5 rounded-full bg-emerald-500 text-white font-medium hover:bg-emerald-600 transition whitespace-nowrap"
       >
         Cari
-      </button>
+      </button> */}
     </div>
   )
 }
